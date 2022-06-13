fad472dfa481dba08341d143a7957e02a6cff4d4
naufalalfikri <naufalalfikri@gmail.com>
2022-06-14 06:58:14 +0700

    sudah dikerjakan tugas 11

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+let xxi = await promiseTheaterIXX();
+let cgv = await promiseTheaterVGC();
+let bioskop = xxi.concat(cgv);
+let hasilEmosi = 0;
+
+bioskop.map(xxi => xxi.hasil === emosi && (hasilEmosi += 1));
+
+return hasilEmosi;
+};
 
 module.exports = {
   promiseOutput,
